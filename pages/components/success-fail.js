3d1c0c17c1817b1df867f10e326d66fdd2ca193e
Anuj <vedanuj@yahoo.com>
2020-10-30 10:22:32 +0530

    add react in all component

--- pages/components/success-fail.js
+++ pages/components/success-fail.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 /**
  *
  * @param {Boolean} status required
--- pages/components/table-lyout.js
+++ pages/components/table-lyout.js
@@ -1,3 +1,4 @@
+import React from "react";
 const TableLyout = ({ label, text }) => {
     return (
         <li>{label} <span>{text}</span></li>
